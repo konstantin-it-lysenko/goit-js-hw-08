00199c794d87004394e751cc2a266298d68037be
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-18 21:26:26 +0300

    complete task 02.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const LS_TIME = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(setTime, 1000));
+
+let time = localStorage.getItem(LS_TIME);
+
+if (time !== null) {
+    player.setCurrentTime(time);
+}
+
+function setTime({ seconds }) {
+    localStorage.setItem(LS_TIME, seconds);
+}
