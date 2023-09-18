cc59076575360edf62301de777ca305d8dc6254b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-18 21:26:39 +0300

    complete task 03.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const LS_FORM_STATE = 'feedback-form-state';
+let formData = {};
+
+populateForm();
+formEl.addEventListener('input', throttle(handleFormInput, 500));
+formEl.addEventListener('submit', handleFormSubmit);
+
+function handleFormInput(event) {
+    formData[event.target.name] = event.target.value;
+
+    const jsonFormData = JSON.stringify(formData);
+    localStorage.setItem(LS_FORM_STATE, jsonFormData);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = formEl.elements;
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+
+    if (!email || !message) return alert('All the fields should be filled');
+
+    console.log(formData);
+
+    localStorage.removeItem(LS_FORM_STATE);
+    event.currentTarget.reset();
+}
+
+function populateForm() {
+    if (localStorage.getItem(LS_FORM_STATE)) {
+        formData = JSON.parse(localStorage.getItem(LS_FORM_STATE));
+
+        for (const key in formData) {
+            if (formData[key]) {
+                formEl.elements[key].value = formData[key];
+            }
+        }
+    }
+}
